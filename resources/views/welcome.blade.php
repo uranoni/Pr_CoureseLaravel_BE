56ba1a34b427f8c90ba839c6e3046223b4008fb0
Roni <uranoni777@gmail.com>
2019-06-14 17:05:23 +0800

    add yeild and section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,8 +5,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
+        
         <!-- Fonts -->
+        <link rel="stylesheet" href="css/app.css">
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Styles -->
