adbf7629fcb79ac35ecdd9a8617f0b1609913045
Roni <uranoni777@gmail.com>
2019-07-02 16:39:30 +0800

    make auth by php artisan make auth

--- routes/web.php
+++ routes/web.php
@@ -48,3 +48,7 @@ Route::delete('/posts/{post}','PostController@destory');
 
 Route::get('/posts/{post}/edit','PostController@edit');
 Route::get('/posts','PostController@index');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
