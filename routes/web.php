0180285e7ca0412828a06660ffc4891e1f11ec07
Roni <uranoni777@gmail.com>
2019-07-02 16:49:58 +0800

    add route middleware group

--- routes/web.php
+++ routes/web.php
@@ -23,27 +23,19 @@ Route::get('/contact', function () {
     return view('contact');
 });
 
-// Route::get('/posts', function () {
-//     $posts=[1,2,3,4,5];
-//     return view('posts.list',['posts'=>$posts]);
-// });
 
-// Route::get('/posts/{id}', function ($id) {
-//     return view('posts.show');
-// });
+Route::group(['middleware' => ['auth']], function () {
+    Route::get('/posts/admin','PostController@admin');
+    Route::get('/posts/create','PostController@create');
+    Route::get('/posts/show/{post}','PostController@show');
 
-Route::get('/posts/admin','PostController@admin');
-//提前擺放路由 會先找create 沒有metch 會往下找post show 的部分
-Route::get('/posts/create','PostController@create');
-Route::get('/posts/show/{post}','PostController@show');
 
-//CRUD
-//3 rountion
-//路由 由上往下找
-Route::post('/posts','PostController@store');
-Route::get('/posts/{post}','PostController@show');
-Route::put('/posts/{post}','PostController@update');
-Route::delete('/posts/{post}','PostController@destory');
+    Route::post('/posts','PostController@store');
+    Route::get('/posts/{post}','PostController@show');
+    Route::put('/posts/{post}','PostController@update');
+    Route::delete('/posts/{post}','PostController@destory');
+});
+
 
 
 Route::get('/posts/{post}/edit','PostController@edit');
