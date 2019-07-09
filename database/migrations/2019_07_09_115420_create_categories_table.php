6d9f8e1500d2adff8f75a461db8dbf4537f874d5
Roni <uranoni777@gmail.com>
2019-07-09 12:51:02 +0800

    add category model table migration

---
+++ database/migrations/2019_07_09_115420_create_categories_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::table('posts',function(Blueprint $table){
+            $table->unsignedBigInteger('category_id')->after('user_id')->nullable();
+            $table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('posts',function(Blueprint $table){
+            $table->dropForeign(['category_id']);
+            $table->dropColumn('category_id');
+        });
+        Schema::dropIfExists('categories');
+    }
+}
